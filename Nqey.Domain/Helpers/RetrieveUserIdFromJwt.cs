9ae70956640a9c965019c25262b5b6e9863a0856
SabirirTheSecond <boukezzoula.sabri@gmail.com>
2026-07-27 20:57:30 +0100

    corrected an error in a helper class not implemented yet

--- Nqey.Domain/Helpers/RetrieveUserIdFromJwt.cs
+++ Nqey.Domain/Helpers/RetrieveUserIdFromJwt.cs
@@ -9,27 +9,29 @@ namespace Nqey.Domain.Helpers
 {
     public class RetrieveUserIdFromJwt
     {
-        public RetrieveUserIdFromJwt() { 
-        
+        public RetrieveUserIdFromJwt()
+        {
+
         }
         public int RetrieveAuthenticatedUserId(Role role)
         {
             return 0;
         }
 
-//        var roleClaim = User.FindFirst(ClaimTypes.Role);
-        
+        //        var roleClaim = User.FindFirst(ClaimTypes.Role);
 
-//            var userIdClaim0 = User.FindFirst("userId")?.Value;
-       
 
-//            var role = roleClaim?.Value;
-       
+        //            var userIdClaim0 = User.FindFirst("userId")?.Value;
 
-//            var userIdClaim = User.FindFirst("userId")?.Value;
-//            if(!int.TryParse(userIdClaim, out var userId) )
-//            {
-//                return NotFound(new ApiResponse<ProviderAdminGetDto>(false, "Not Found"));   
-//            }
-//}
+
+        //            var role = roleClaim?.Value;
+
+
+        //            var userIdClaim = User.FindFirst("userId")?.Value;
+        //            if(!int.TryParse(userIdClaim, out var userId) )
+        //            {
+        //                return NotFound(new ApiResponse<ProviderAdminGetDto>(false, "Not Found"));   
+        //            }
+        //}
+    }
 }
